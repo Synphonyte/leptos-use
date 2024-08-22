f328ee8e71eb4aa418bc320d481366edca7d581f
Baptiste de Montangon <baptiste.demontangon@gmail.com>
2024-08-22 16:51:31 +0200

    forget the unmount handle

--- examples/use_interval_fn/src/main.rs
+++ examples/use_interval_fn/src/main.rs
@@ -66,7 +66,9 @@ fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
-    let _ = leptos::mount::mount_to(demo_or_body(), || {
+    let unmount_handle = leptos::mount::mount_to(demo_or_body(), || {
         view! { <Demo/> }
     });
+
+    unmount_handle.forget();
 }
