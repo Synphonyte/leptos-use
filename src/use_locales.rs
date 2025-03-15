aa71720ea3fd0bcaec0d7730025ea5c08c677500
Marc-Stefan Cassola <maccesch@synphonyte.com>
2025-03-15 17:44:04 +0000

    Update src/use_locales.rs

--- src/use_locales.rs
+++ src/use_locales.rs
@@ -38,7 +38,7 @@ use std::sync::Arc;
 ///
 /// ### Bring your own header
 ///
-/// In case you're neither using Axum, Actix or the default implementation is not to your liking,
+/// In case you're neither using Axum nor Actix or the default implementation is not to your liking,
 /// you can provide your own way of reading the language header value using the option
 /// [`crate::UseLocalesOptions::ssr_lang_header_getter`].
 pub fn use_locales() -> Signal<Vec<String>> {
