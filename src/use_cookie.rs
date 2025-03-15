2ac08d07ca88b34f61de5fbad683c8fdde5aabee
Marc-Stefan Cassola <maccesch@synphonyte.com>
2025-03-15 17:41:40 +0000

    Update src/use_cookie.rs

--- src/use_cookie.rs
+++ src/use_cookie.rs
@@ -103,7 +103,7 @@ use std::sync::Arc;
 ///
 /// ### Bring your own header
 ///
-/// In case you're neither using Axum, Actix or the default implementation is not to your liking,
+/// In case you're neither using Axum nor Actix or the default implementation is not to your liking,
 /// you can provide your own way of reading and writing the cookie header value.
 ///
 /// ```
