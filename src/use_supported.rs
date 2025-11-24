935a0907800b27120fd414f908cc835b3ca6a01b
DrFlowerkick <flowerkick14@gmail.com>
2025-11-24 17:15:37 +0100

    fix: :bug: Fix possible hydration errors with use_supported by calling the callback AFTER mount of reactive system.

--- src/use_supported.rs
+++ src/use_supported.rs
@@ -29,6 +29,15 @@ pub fn use_supported(callback: impl Fn() -> bool + Send + Sync + 'static) -> Sig
 
     #[cfg(not(feature = "ssr"))]
     {
-        Signal::derive(callback)
+        // make sure we do not create hydration errors by calling the callback, when the client is mounted.
+
+        // 1. create a signal that tracks if we are mounted
+        let (is_mounted, set_mounted) = signal(false);
+
+        // 2. create an effect that sets is_mounted to true on mount
+        Effect::new(move |_| set_mounted.set(true));
+
+        // 3. create a derived signal that calls the callback only when mounted
+        Signal::derive(move || if is_mounted.get() { callback() } else { false })
     }
 }
