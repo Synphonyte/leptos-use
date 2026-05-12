0a94a56d1a50b4c444607494629baba88f8fa38f
M. Yas. Davoodeh <MYDavoodeh@gmail.com>
2026-05-12 19:33:23 +0330

    Add some comments to `utils/js` modules

--- src/utils/js.rs
+++ src/utils/js.rs
@@ -1,3 +1,13 @@
+//! Holds JavaScript utilities and especially the [`js`] macro.
+
+/// Expand to common JavaScript operations that are too long in Rust.
+///
+/// Holds three primary rules:
+/// - `attribute in object`: Check if the attribute is in the object (see [`JsValue::js_in`]).
+/// - `object[attribute]`: Get the value of the object attribute (see [`Reflect::get`]).
+/// - `object[attribute] = val`: Assign to the attribute of the object (see [`Reflect::set`]).
+///
+/// [`JsValue`]: wasm_bindgen::JsValue
 #[macro_export]
 macro_rules! js {
     ($attr:literal in $($obj:tt)*) => {
@@ -13,6 +23,7 @@ macro_rules! js {
     };
 }
 
+/// Create a [`wasm_bindgen_futures::JsFuture`] from the given tokens.
 #[macro_export]
 macro_rules! js_fut {
     ($($obj:tt)*) => {
